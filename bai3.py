49f26d61ae6040520896d5bf501f0bd29b3bc295
Duong Ngoc Hieu <hieubeo1538@gmail.com>
2025-05-20 22:42:54 +0700

    Create bai3.py

---
+++ bai3.py
@@ -0,0 +1,26 @@
+def binh_phuong_co_lap(a,k,n):
+    b=1
+    if k==0:
+        return b
+    A=a
+    k_bin=bin(k)[2:]# chuyen k sang chuoi nhi phan
+    t=len(k_bin)
+
+    if k_bin[-1]== '1':
+        b=a 
+
+    for i in range(1,t):
+        A=(A*A)%n
+        if k_bin[-1-i]=='1':
+            b=(b*A)%n
+
+    return b
+
+if __name__=='__main__':
+    p=int(input('NHAP SO P:'))
+    q=int(input('NHAP VAO q:'))
+    m=int(input('NHAP VAO m:'))
+
+    n=p*q
+    c=binh_phuong_co_lap(m,2,n)
+    print('ma hoa thong diep m: ',c)
